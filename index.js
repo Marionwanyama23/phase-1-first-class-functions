df2887620746e4f41b5bb9953b6ed34e4b3fe491
+ <+>
2022-06-11 14:39:45 +0300

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction (callback) {
+    console.log(callback());
+}
+function returnsANamedFunction() {
+    const fn = function () {};
+return fn;
+}
+function returnsAnAnonymousFunction () {
+    return function() {};
+}
